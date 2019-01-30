1dcc3e59ee81f59aae6e98994c17b0de76b3cab6
Yonas21 <yonasalem056@gmail.com>
2019-01-30 14:55:25 +0300

    to push to git

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,36 +1,98 @@
 <template>
-  <div id="about">
-    <span>{{name}}</span>
-  </div>
+    <div id="about">
+        <span v-once> Message: {{message}}</span>
+        <p>Using v-html directive: <span style = "color: green" v-html="message"></span></p>
+        <hr/>
+        <button @click="showSome" :disabled="check">Submit</button>
+        <hr>
+        <span v-if="seen">This block should be seen.</span>
+        <ol>
+            <li v-for="(data,index) in users" :key="index">{{data.name}}</li>
+        </ol>
+
+        <!--<div class="container">-->
+        <!--<label for="username" class="v-label">User Name</label>-->
+        <!--<input type="text" class="form-control" v-model="name">-->
+        <!--</div>-->
+
+        <!--<div class="list-item">-->
+            <!--<v-list-tile-title v-for="item in items">{{item}}</v-list-tile-title>-->
+        <!--</div>-->
+
+        <div class="container">
+            <p>The Original user name: {{userName}}</p>
+            <p>The Reversed user name: {{reversedUserName}}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-/*eslint-disable */
-export default {
-  name: 'about',
-  data(){
-    return {
-      name:'Yonas'
+    /*eslint-disable */
+
+    //Object.freeze(object)
+    export default {
+        name: 'about',
+        data() {
+            return {
+                seen:true,
+                check:true,
+                message: 'This is the message from Angular.',
+                users:[
+                    {name:'Yonas'},
+                    {name: 'Same'},
+                    {name:'Mike'},
+                    {name:'Hiskel'}
+                ],
+                items:[
+                    "welcome to html",
+                    "style ur page ",
+                    "make it interactive with js",
+                    "try some frameworks",
+                    "Angular",
+                    "Express",
+                    "React"
+            ],
+                userName:'Lanzini'
+            }
+        },
+        methods:{
+            showSome(){
+                console.log("welcome to vue event handling")
+            }
+        },
+        computed: {
+            reversedUserName:function () {
+                //reverse the userName from the data item above
+                return this.userName.split('').reverse().join('')
+            }
+        }
     }
-  }  
-  
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+
+    ol {
+        font-size: medium;
+    }
+
+    ol li {
+        list-style-type:lower-alpha;
+        display:block;
+        color: crimson;
+    }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -11,7 +11,8 @@ export default {
     name:'about',
     components:{
         About
-    }
+    },
+
 }
 </script>
 
